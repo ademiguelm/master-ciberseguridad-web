1b71e6fab94aa30e02052d0ca40388642ddf814e
ademiguelm <38988458+ademiguelm@users.noreply.github.com>
2018-05-04 19:46:28 +0200

    Update PublicContentBase.java
    Se ha modificado este código como resultado de la ejecución de la Tarea 3 del Módulo 8.

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -10,11 +10,15 @@ public class PublicContentBase extends Controller {
     public static void register(){
         render();
     }
-
+    
+    //Modificado por Angeles de Miguel para Modulo8 Tarea4, de forma que username y password han de tener 
+    //al menos una longitud de 8 caracteres. Tambien se comprueba que username y password no sean iguales
     public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
-        u.save();
-        registerComplete();
+        if (username.length()>=8 and password.length()>=8 and !username.equalsIgnoreCase(password)){
+            User u = new User(username, HashUtils.getMd5(password), type, -1);
+            u.save();
+            registerComplete();
+        }
     }
 
     public static void registerComplete(){
